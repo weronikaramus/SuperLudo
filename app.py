0a3bbd76d76c6e6b5583a16f7375a34782091db6
Weronika <you@example.com>
2024-02-04 17:23:20 +0100

    move and kill pawns

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for, jsonify, session, request
 from datetime import datetime
-from pawn import red_pawns, Pawn
+from pawn import pawns
 from dice import Dice
 import random
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+
+colors = ["red", "blue", "yellow", "green"]
 
 def rollDice():
     dice_result = random.randint(1, 6)
@@ -21,22 +23,21 @@ def index():
 def game():
     roll_result = None
     result = None
+    index = None
+    color = None
     if request.method == 'POST':
         if request.form['submit'] == 'rollDice':
             roll_result = rollDice()
         if request.form['submit'].startswith('move_'):
-            index = int(request.form['submit'].split('_')[1])-1
             if 'dice_result' in session:
-                result = red_pawns[index].move(session['dice_result'])
+                index = int(request.form['submit'].split('_')[2])
+                color = colors.index(request.form['submit'].split('_')[1])
+                print(index, color)
+                result = pawns[color][index].move(session['dice_result'])
                 session.pop('dice_result')
 
-    return render_template('game.html', red_pawns=red_pawns, roll_result=roll_result, result=result)
+    return render_template('game.html', pawns=pawns, roll_result=roll_result, result=result)
 
-# @app.route('/roll_dice', methods=['POST'])
-# def roll_dice():
-#     rollDice()
-
-#     return jsonify({'result': dice_result})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- pawn.py
+++ pawn.py
@@ -4,23 +4,52 @@ from flask import Flask, render_template, session
 app = Flask(__name__)
 len = 52
 class Pawn:
-    def __init__(self, color, position):
+    def __init__(self, color):
+        position = 0
+        start = 0
         self.color = color
         self.position = position
+        self.start = start
+        if self.color == "red":
+            self.start = 0
+        if self.color == "blue":
+            self.start = len/2
+
+
+        self.position = self.start
         # self.superpower = superpower
 
+    def kill(self, other_pawn):
+        other_pawn.position = other_pawn.start
+
     def move(self, steps):
+        current_position = self.position
         steps = session.get('dice_result', 0)
-        self.position = (self.position+steps)%len
+        new_position = (current_position + steps) % len
+
+        # Check if the new position overlaps with any other pawn's position
+        for row in pawns:
+            for other_pawn in row:
+                if other_pawn.position == new_position and other_pawn != self:
+                    # Call the "kill" function when a collision is detected
+                    self.kill(other_pawn)
+
+        self.position = new_position
+
+    # def move(self, steps):
+    #     steps = session.get('dice_result', 0)
+    #     new_position = (self.position+steps)%len
+    #     for row in pawns:
+    #         for other_pawn in row:
+    #             if other_pawn.position == new_position and other_pawn != self and other_pawn.position != other_pawn.start:
+    #                 self.kill(other_pawn)
+    #     self.position = new_position
 
 
 
-red_pawn1 = Pawn("red", 0)
-red_pawn2 = Pawn("red", 0)
-# red_pawn3 = Pawn("red", 0)
-# red_pawn4 = Pawn("red", 0)
-red_pawns = [red_pawn1, red_pawn2]
-blue_pawn1 = Pawn("blue", len/2)
-blue_pawn2 = Pawn("blue", len/2)
-# blue_pawn3 = Pawn("blue", len/2)
-# blue_pawn4 = Pawn("blue", len/2)
+
+pawns = [[Pawn("Empty") for _ in range(2)] for _ in range(2)]
+pawns[0][0] = Pawn("red")
+pawns[0][1] = Pawn("red")
+pawns[1][0] = Pawn("blue")
+pawns[1][1] = Pawn("blue")
